52b3f0bac5a5e2ab71f8616afc16a59e3a333524
Maślany Wojciech <wojciech.maslany1@gmail.com>
2019-11-24 17:32:38 +0100

    More JS

--- 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie02.js
@@ -4,6 +4,42 @@ let Calculator = function() {
 
 Calculator.prototype.add = function(num1, num2) {
     var result = num1 + num2;
-    this.history.push
+    this.history.push (`added ${num1} to ${num2} got ${result}`)
     return result;
 }
+
+Calculator.prototype.multiply = function(num1, num2) {
+    var result = num1 * num2;
+    this.history.push (`multiplied ${num1} with ${num2} got ${result}`)
+    return result;
+}
+
+Calculator.prototype.substract = function(num1, num2) {
+    var result = num1 - num2;
+    this.history.push (`substracted ${num1} from ${num2} got ${result}`)
+    return result;
+}
+
+Calculator.prototype.divide = function(num1, num2) {
+    var result = num1 + num2;
+    this.history.push (`divided ${num1} by ${num2} got ${result}`)
+    return result;
+}
+
+Calculator.prototype.printOperation = function () {
+    console.table(this.history);
+}
+
+Calculator.prototype.clearOperation = function () {
+    this.history = [];
+}
+
+var kalkulator = new Calculator();
+
+kalkulator.add(1,1)
+kalkulator.multiply(2,2)
+kalkulator.substract(10, 5)
+kalkulator.divide(3, 3)
+kalkulator.printOperation();
+kalkulator.clearOperation();
+kalkulator.printOperation();
